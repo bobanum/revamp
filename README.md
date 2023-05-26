2e1c35d7394ff54883fedb40614fef528d7e6ffd
Martin Boudreau <mboudrea\cstj.qc.ca>
2023-05-26 00:47:44 -0400

    First working version

--- README.md
+++ README.md
@@ -1,15 +1,160 @@
-<img src="/art/logo_revamp.svg" alt="Logo Revamp">
+# ![Revamp Signature](art/signature_revamp.svg)
+A better way to find your files in Laravel!
 
-<p align="center">
-    <a href="https://packagist.org/packages/bobanum/revamp">
-        <img src="https://img.shields.io/packagist/dt/bobanum/revamp" alt="Total Downloads">
-    </a>
-    <a href="https://packagist.org/packages/bobanum/revamp">
-        <img src="https://img.shields.io/packagist/v/bobanum/revamp" alt="Latest Stable Version">
-    </a>
-    <a href="https://packagist.org/packages/bobanum/revamp">
-        <img src="https://img.shields.io/packagist/l/bobanum/revamp" alt="License">
-    </a>
-</p>
+[![Total Downloads](https://img.shields.io/packagist/dt/bobanum/revamp)](https://packagist.org/packages/bobanum/revamp)
+[![Latest Stable Version](https://img.shields.io/packagist/v/bobanum/revamp)](https://packagist.org/packages/bobanum/revamp)
+[![License](https://img.shields.io/packagist/l/bobanum/revamp)](https://packagist.org/packages/bobanum/revamp)
+
 
 ## Introduction
+Revamp takes your normal files hierarchy :
+```
+📦my-laravel-project
+ ┣ 📂app
+ ┃ ┣ 📂Http
+ ┃ ┃ ┣ 📂Controllers
+ ┃ ┃ ┃ ┣ 📜Controller.php
+ ┃ ┃ ┃ ┣ 📜DepartmentController.php
+ ┃ ┃ ┃ ┣ 📜SchoolController.php
+ ┃ ┃ ┃ ┗ 📜TeacherController.php
+ ┃ ┃ ┗ 📂Requests
+ ┃ ┃   ┣ 📜StoreDepartmentRequest.php
+ ┃ ┃   ┣ 📜StoreSchoolRequest.php
+ ┃ ┃   ┣ 📜StoreTeacherRequest.php
+ ┃ ┃   ┣ 📜UpdateDepartmentRequest.php
+ ┃ ┃   ┣ 📜UpdateSchoolRequest.php
+ ┃ ┃   ┗ 📜UpdateTeacherRequest.php
+ ┃ ┣ 📂Models
+ ┃ ┃ ┣ 📜Department.php
+ ┃ ┃ ┣ 📜School.php
+ ┃ ┃ ┣ 📜Teacher.php
+ ┃ ┃ ┗ 📜User.php
+ ┃ ┗ 📂Policies
+ ┃   ┣ 📜DepartmentPolicy.phpz
+ ┃   ┣ 📜SchoolPolicy.php
+ ┃   ┗ 📜TeacherPolicy.php
+ ┣ 📂concepts
+ ┃ ┣ 📂Department
+ ┃ ┃ ┣ 📜Controller.php
+ ┃ ┃ ┣ 📜Factory.php
+ ┃ ┃ ┣ 📜migration.php
+ ┃ ┃ ┣ 📜Model.php
+ ┃ ┃ ┣ 📜Seeder.php
+ ┃ ┃ ┣ 📜StoreRequest.php
+ ┃ ┃ ┗ 📜UpdateRequest.php
+ ┃ ┣ 📂School
+ ┃ ┃ ┣ 📂views
+ ┃ ┃ ┃ ┗ 📜index.blade.php
+ ┃ ┃ ┣ 📜Controller.php
+ ┃ ┃ ┣ 📜Factory.php
+ ┃ ┃ ┣ 📜migration.php
+ ┃ ┃ ┣ 📜Model.php
+ ┃ ┃ ┣ 📜Policy.php
+ ┃ ┃ ┣ 📜Seeder.php
+ ┃ ┃ ┣ 📜StoreRequest.php
+ ┃ ┃ ┗ 📜UpdateRequest.php
+ ┃ ┣ 📂Teacher
+ ┃ ┃ ┣ 📜Controller.php
+ ┃ ┃ ┣ 📜Factory.php
+ ┃ ┃ ┣ 📜migration.php
+ ┃ ┃ ┣ 📜Model.php
+ ┃ ┃ ┣ 📜Policy.php
+ ┃ ┃ ┣ 📜Seeder.php
+ ┃ ┃ ┣ 📜StoreRequest.php
+ ┃ ┃ ┗ 📜UpdateRequest.php
+ ┃ ┗ 📂User
+ ┃   ┣ 📜Factory.php
+ ┃   ┣ 📜migration.php
+ ┃   ┗ 📜Model.php
+ ┣ 📂database
+ ┃ ┣ 📂factories
+ ┃ ┃ ┣ 📜DepartmentFactory.php
+ ┃ ┃ ┣ 📜SchoolFactory.php
+ ┃ ┃ ┣ 📜TeacherFactory.php
+ ┃ ┃ ┗ 📜UserFactory.php
+ ┃ ┣ 📂migrations
+ ┃ ┃ ┣ 📜2014_10_12_000000_create_users_table.php
+ ┃ ┃ ┣ 📜2023_05_25_234856_create_schools_table.php
+ ┃ ┃ ┣ 📜2023_05_25_234904_create_departments_table.php
+ ┃ ┃ ┗ 📜2023_05_25_234911_create_teachers_table.php
+ ┃ ┗ 📂seeders
+ ┃   ┣ 📜DatabaseSeeder.php
+ ┃   ┣ 📜DepartmentSeeder.php
+ ┃   ┣ 📜SchoolSeeder.php
+ ┃   ┗ 📜TeacherSeeder.php
+ ┣ 📂resources
+ ┃ ┗ 📂views
+ ┃   ┗ 📂school
+ ┃     ┣ 📜index.blade.php
+ ┃     ┗ 📜show.blade.php
+ ┗ 📂routes
+   ┣ 📜api.php
+   ┣ 📜channels.php
+   ┣ 📜console.php
+   ┗ 📜web.php
+```
+And adds a __concepts__ folder filled with __links__ to the important files sorted and grouped by model.
+Every change made to thoses files will affect the target file since they are the same file.
+
+> For example, the model `/app/Models.School.php` is the same file as `concepts/School/Model.php`. Easier to find, easier to maintain.
+```
+📦my-laravel-project
+ ┣ 📂concepts
+ ┃ ┣ 📂Department
+ ┃ ┃ ┣ 📜Controller.php
+ ┃ ┃ ┣ 📜Factory.php
+ ┃ ┃ ┣ 📜migration.php
+ ┃ ┃ ┣ 📜Model.php
+ ┃ ┃ ┣ 📜Seeder.php
+ ┃ ┃ ┣ 📜StoreRequest.php
+ ┃ ┃ ┗ 📜UpdateRequest.php
+ ┃ ┣ 📂School
+ ┃ ┃ ┣ 📂views
+ ┃ ┃ ┃ ┣ 📜index.blade.php
+ ┃ ┃ ┃ ┗ 📜show.blade.php
+ ┃ ┃ ┣ 📜Controller.php
+ ┃ ┃ ┣ 📜Factory.php
+ ┃ ┃ ┣ 📜migration.php
+ ┃ ┃ ┣ 📜Model.php
+ ┃ ┃ ┣ 📜Policy.php
+ ┃ ┃ ┣ 📜Seeder.php
+ ┃ ┃ ┣ 📜StoreRequest.php
+ ┃ ┃ ┗ 📜UpdateRequest.php
+ ┃ ┣ 📂Teacher
+ ┃ ┃ ┣ 📜Controller.php
+ ┃ ┃ ┣ 📜Factory.php
+ ┃ ┃ ┣ 📜migration.php
+ ┃ ┃ ┣ 📜Model.php
+ ┃ ┃ ┣ 📜Policy.php
+ ┃ ┃ ┣ 📜Seeder.php
+ ┃ ┃ ┣ 📜StoreRequest.php
+ ┃ ┃ ┗ 📜UpdateRequest.php
+ ┃ ┗ 📂User
+ ┃   ┣ 📜Factory.php
+ ┃   ┣ 📜migration.php
+ ┃   ┗ 📜Model.php
+ ┗ 📂database
+   ┣ 📂factories
+   ┃ ┣ 📜DepartmentFactory.php
+   ┃ ┣ 📜SchoolFactory.php
+   ┃ ┣ 📜TeacherFactory.php
+   ┃ ┗ 📜UserFactory.php
+   ┣ 📂migrations
+   ┃ ┣ 📜2014_10_12_000000_create_users_table.php
+   ┃ ┣ 📜2023_05_25_234856_create_schools_table.php
+   ┃ ┣ 📜2023_05_25_234904_create_departments_table.php
+   ┃ ┗ 📜2023_05_25_234911_create_teachers_table.php
+   ┗ 📂seeders
+     ┣ 📜DatabaseSeeder.php
+     ┣ 📜DepartmentSeeder.php
+     ┣ 📜SchoolSeeder.php
+     ┗ 📜TeacherSeeder.php
+```
+
+<style>
+    h1+p, h1+p+p {
+        display:flex;
+        justify-content: center;
+        gap: 1ch;
+    }
+</style>
--- src/Console/RevampCommand.php
+++ src/Console/RevampCommand.php
@@ -3,34 +3,187 @@
 namespace Bobanum\Revamp\Console;
 
 use Illuminate\Console\Command;
-// use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
 
-class RevampCommand extends Command
-{
+class RevampCommand extends Command {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'revamp:revamp';
+    protected $signature = 'revamp';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Install the Revamp controllers and resources';
+    protected $description = 'Revamp the application';
 
     /**
      * Execute the console command.
      *
      * @return int|null
      */
-    public function handle()
-    {
+    public function handle() {
+        $this->info('Installing Revamp...');
+        $this->makDirIfNotExist(base_path('concepts'));
+        $concepts = $this->getConcepts();
+        $this->revamp($concepts);
         return 1;
     }
-
-    
-
+    public function makDirIfNotExist($directory) {
+        if (!is_dir($directory)) {
+            mkdir($directory, 0755, true);
+            $this->info('Created Directory: ' . substr($directory, strlen(base_path()) + 1));
+        }
+    }
+    public function getConcepts() {
+        // Get all the models
+        $files = glob(app_path('Models/*.php'));
+        $concepts = [];
+        foreach ($files as $file) {
+            $concept = str_replace('.php', '', basename($file));
+            if ($concept === 'Model') {
+                continue;
+            }
+            $concepts[] = $concept;
+        }
+        // Get all the controllers
+        $files = glob(app_path('Http/Controllers/*.php'));
+        foreach ($files as $file) {
+            $concept = str_replace('Controller.php', '', basename($file));
+            if (!$concept) {
+                continue;
+            }
+            if (!isset($concepts[$concept])) {
+                $concepts[] = $concept;
+            }
+        }
+        return $concepts;
+    }
+    public function linkFileIfNeeded($source, $destination) {
+        if (!file_exists($destination)) {
+            link($source, $destination);
+            $this->info('Linked: ' . substr($destination, strlen(base_path()) + 1), 'vv');
+        } else {
+            $this->info('Skipped: ' . substr($destination, strlen(base_path()) + 1), 'vv');
+        }
+    }
+    public function linkDirIfNeeded($source, $destination) {
+        if (is_dir($source)) {
+            shell_exec("ln -s \"$source\" \"$destination\"");
+            $this->info('Linked: ' . substr($destination, strlen(base_path()) + 1), 'vv');
+        } else {
+            $this->info('Skipped: ' . substr($destination, strlen(base_path()) + 1), 'vv');
+        }
+    }
+    public function revamp($concepts) {
+        foreach ($concepts as $concept) {
+            $this->info('===> Revamping: ' . $concept);
+            $this->makDirIfNotExist(base_path('concepts/' . $concept));
+            $this->revampModel($concept);
+            $this->revampController($concept);
+            $this->revampMigration($concept);
+            $this->revampSeeder($concept);
+            $this->revampFactory($concept);
+            $this->revampPolicy($concept);
+            $this->revampRequests($concept);
+            $this->revampViews($concept);
+            $this->revampRoutes($concept);
+        }
+    }
+    public function revampModel($concept) {
+        $path = app_path('Models/' . $concept . '.php');
+        $link = base_path('concepts/' . $concept . '/Model.php');
+        $this->info('Revamping Model: ' . $concept);
+        if (!file_exists($path)) {
+            $this->info('Model not found: ' . $concept);
+        } else {
+            $this->linkFileIfNeeded($path, $link);
+        }
+    }
+    public function revampController($concept) {
+        $path = app_path('Http/Controllers/' . $concept . 'Controller.php');
+        $link = base_path('concepts/' . $concept . '/Controller.php');
+        $this->info('Revamping Controller: ' . $concept);
+        if (!file_exists($path)) {
+            $this->info('Controller not found: ' . $concept);
+        } else {
+            $this->linkFileIfNeeded($path, $link);
+        }
+    }
+    public function revampMigration($concept) {
+        $path = glob(database_path('migrations/*_create_' . Str::plural(Str::snake($concept)) . '_table.php'))[0];
+        $link = base_path('concepts/' . $concept . '/migration.php');
+        $this->info('Revamping Migration: ' . $concept);
+        if (!file_exists($path)) {
+            $this->info('Migration not found: ' . $concept);
+        } else {
+            $this->linkFileIfNeeded($path, $link);
+        }
+    }
+    public function revampSeeder($concept) {
+        $path = database_path('seeders/' . $concept . 'Seeder.php');
+        $link = base_path('concepts/' . $concept . '/Seeder.php');
+        $this->info('Revamping Seeder: ' . $concept);
+        if (!file_exists($path)) {
+            $this->info('Seeder not found: ' . $concept);
+        } else {
+            $this->linkFileIfNeeded($path, $link);
+        }
+    }
+    public function revampFactory($concept) {
+        $path = database_path('factories/' . $concept . 'Factory.php');
+        $link = base_path('concepts/' . $concept . '/Factory.php');
+        $this->info('Revamping Factory: ' . $concept);
+        if (!file_exists($path)) {
+            $this->info('Factory not found: ' . $concept);
+        } else {
+            $this->linkFileIfNeeded($path, $link);
+        }
+    }
+    public function revampPolicy($concept) {
+        $path = app_path('Policies/' . $concept . 'Policy.php');
+        $link = base_path('concepts/' . $concept . '/Policy.php');
+        $this->info('Revamping Policy: ' . $concept);
+        if (!file_exists($path)) {
+            $this->warn('Policy not found: ' . $concept, 'vv');
+        } else {
+            $this->linkFileIfNeeded($path, $link);
+        }
+    }
+    public function revampRequests($concept) {
+        $paths = glob(app_path('Http/Requests/*' . $concept . 'Request.php'));
+        $this->info('Revamping Requests: ' . $concept);
+        foreach ($paths as $path) {
+            $name = basename($path);
+            $name = str_replace($concept, '', $name);
+            $link = base_path('concepts/' . $concept . '/' . $name);
+            $this->linkFileIfNeeded($path, $link);
+        }
+    }
+    public function revampViews($concept) {
+        $path = resource_path('views/' . Str::plural(Str::snake($concept)));
+        if (!file_exists($path)) {
+            $path = resource_path('views/' . Str::snake($concept));
+        }
+        $link = base_path('concepts/' . $concept . '/views');
+        $this->info('Revamping Views: ' . $path);
+        if (!file_exists($path)) {
+            $this->warn('Views not found: ' . $concept, 'vv');
+        } else {
+            $this->linkDirIfNeeded($path, $link);
+        }
+    }
+    public function revampRoutes($concept) {
+        $path = base_path('routes/' . $concept . '.php');
+        $link = base_path('concepts/' . $concept . '/routes.php');
+        $this->info('Revamping Routes: ' . $concept);
+        if (!file_exists($path)) {
+            $this->warn('Routes not found: ' . $concept, 'vv');
+        } else {
+            $this->linkFileIfNeeded($path, $link);
+        }
+    }
 }
